d7721e373186544b1b7b097c2de294dd7d54721e
imvaibhav100 <imvaibhavshukla@gamil.com>
2026-09-17 18:12:48 +0530

    new updated code

--- InstaAstro/urls.py
+++ InstaAstro/urls.py
@@ -3,5 +3,6 @@ from django.urls import path, include
 #new coments are added
 urlpatterns = [
     path("admin/", admin.site.urls),
+    path("", include("InstaAstro.urls")),
     path("", include("Career_page.urls")),
 ]
