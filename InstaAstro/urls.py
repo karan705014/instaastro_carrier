f5cedfc5563b0a4fd34f7d9bbc76ebb3efdda12e
imvaibhav100 <imvaibhavshukla@gamil.com>
2026-09-17 19:12:18 +0530

    url fix

--- InstaAstro/urls.py
+++ InstaAstro/urls.py
@@ -1,12 +1,8 @@
 from django.contrib import admin
 from django.urls import path, include
-<<<<<<< HEAD
 #new coments are added
-=======
 #sthese are URl end points
->>>>>>> e1ac666 (add some comments)
 urlpatterns = [
     path("admin/", admin.site.urls),
-    path("", include("InstaAstro.urls")),
     path("", include("Career_page.urls")),
 ]
