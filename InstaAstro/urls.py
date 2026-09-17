e1ac66605808d407d0febe22cf615162f6c40085
imvaibhav100 <imvaibhavshukla@gamil.com>
2026-09-17 17:33:27 +0530

    add some comments

--- InstaAstro/urls.py
+++ InstaAstro/urls.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 from django.urls import path, include
-
+#sthese are URl end points
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("", include("Career_page.urls")),
